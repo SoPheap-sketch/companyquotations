ab4c66b7a20cf603ad9e05a444679430ff237424
SoPheap-sketch <chhannsopheap493@gmail.com>
2026-03-19 16:34:23 +0700

    remove wkhtmltopdf config

--- app/pdf_utils.py
+++ app/pdf_utils.py
@@ -1,11 +1,4 @@
-# app/pdf_utils.py
 import pdfkit
-from io import BytesIO
 
-WKHTMLTOPDF_PATH = r"C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe"
-
-config = pdfkit.configuration(wkhtmltopdf=WKHTMLTOPDF_PATH)
-
-def render_pdf_from_html(html: str):
-    pdf_bytes = pdfkit.from_string(html, False, configuration=config)
-    return BytesIO(pdf_bytes)
+def render_pdf_from_html(html, output_path):
+    pdfkit.from_string(html, output_path)
