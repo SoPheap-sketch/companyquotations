540531936fc8a056a9bd277c32cfae10416ebbc9
SoPheap-sketch <chhannsopheap493@gmail.com>
2026-03-19 16:58:05 +0700

    add database url for render

--- app/db.py
+++ app/db.py
@@ -1,15 +1,32 @@
-# app/db.py
+# # app/db.py
+# from sqlalchemy import create_engine
+# from sqlalchemy.orm import sessionmaker, declarative_base
+# from pathlib import Path
+
+# BASE_DIR = Path(__file__).resolve().parent.parent
+# DB_PATH = BASE_DIR / "data" / "app.db"
+
+# SQLALCHEMY_DATABASE_URL = f"sqlite:///{DB_PATH}"
+
+# engine = create_engine(
+#     SQLALCHEMY_DATABASE_URL,
+#     connect_args={"check_same_thread": False}
+# )
+
+# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# Base = declarative_base()
+
+# def init_db():
+#     from app import models
+#     Base.metadata.create_all(bind=engine, checkfirst=True)
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from pathlib import Path
+import os
 
-BASE_DIR = Path(__file__).resolve().parent.parent
-DB_PATH = BASE_DIR / "data" / "app.db"
-
-SQLALCHEMY_DATABASE_URL = f"sqlite:///{DB_PATH}"
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:////tmp/app.db")
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL,
+    DATABASE_URL,
     connect_args={"check_same_thread": False}
 )
 
