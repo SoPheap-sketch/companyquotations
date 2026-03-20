b73c22c11e55e2fea22d08e5c18d0c69a9d5d6bf
SoPheap-sketch <chhannsopheap493@gmail.com>
2026-03-20 08:44:49 +0700

    use old database on render

--- app/db.py
+++ app/db.py
@@ -19,9 +19,28 @@
 # def init_db():
 #     from app import models
 #     Base.metadata.create_all(bind=engine, checkfirst=True)
+# from sqlalchemy import create_engine
+# from sqlalchemy.orm import sessionmaker, declarative_base
+# import os
+
+# DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:////tmp/app.db")
+
+# engine = create_engine(
+#     DATABASE_URL,
+#     connect_args={"check_same_thread": False}
+# )
+
+# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# Base = declarative_base()
+
+# def init_db():
+#     from app import models
+#     Base.metadata.create_all(bind=engine, checkfirst=True)
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
+import shutil
 
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:////tmp/app.db")
 
@@ -33,6 +52,13 @@ engine = create_engine(
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
+
 def init_db():
     from app import models
+
+    #  COPY OLD DB INTO /tmp (ONLY FIRST TIME)
+    if not os.path.exists("/tmp/app.db"):
+        if os.path.exists("data/app.db"):
+            shutil.copy("data/app.db", "/tmp/app.db")
+
     Base.metadata.create_all(bind=engine, checkfirst=True)
