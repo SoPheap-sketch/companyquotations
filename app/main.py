3facdb8f6dc65de1bb9cf5ee7cbc4a23c2613d99
SoPheap-sketch <chhannsopheap493@gmail.com>
2026-03-20 09:55:33 +0700

    add admin auto create

--- app/main.py
+++ app/main.py
@@ -11,6 +11,9 @@ from app.routes import admin, attachments, work_instructions
 from app.routes.notifications import router as notifications_router
 from app.routes import invoices
 
+from app.db import SessionLocal
+from app.models import User
+from app.auth import get_password_hash
 
 app = FastAPI(title="Company Quotation System")
 
@@ -22,9 +25,29 @@ app.add_middleware(
 # Static files
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
+# @app.on_event("startup")
+# def startup_event():
+#     _db.init_db()
 @app.on_event("startup")
 def startup_event():
     _db.init_db()
+
+    #  Create admin user if not exists
+    db = SessionLocal()
+    user = db.query(User).filter(User.username == "admin").first()
+
+    if not user:
+        new_user = User(
+            username="admin",
+            email="admin@test.com",
+            hashed_password=get_password_hash("123456"),
+            role="admin"
+        )
+        db.add(new_user)
+        db.commit()
+        print(" Admin created")
+
+    db.close()
 
 # Routers
 app.include_router(pages_router)
